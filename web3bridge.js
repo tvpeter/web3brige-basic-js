e80146bd9476ef3ca21be881774aa86b24e3d8ad
tvpeter <withtvpeter@gmail.com>
2021-03-08 22:11:12 +0100

    add addition, multiplication, substraction and division basic functions for two numbers input

---
+++ web3bridge.js
@@ -0,0 +1,23 @@
+
+
+function addNumbers(a=0, b=0)
+{
+    return Number(a) + Number(b);
+}
+
+
+function subtractNumbers(a=0, b=0)
+{
+    return Number(a) - Number(b);
+}
+
+function multiplyNumbers(a=0, b=0)
+{
+    return Number(a) * Number(b);
+}
+
+function divideNumbers(a=0, b=0)
+{
+    return Number(a) / Number(b);
+}
+
